0df1c6ae6d0e8b94b0b69212f9911f16468eb644
CodeXRook <lacekitfly@gmail.com>
2019-09-22 06:15:00 -0400

    var elMsg

--- c03/js/date-object-difference.js
+++ c03/js/date-object-difference.js
@@ -3,3 +3,5 @@ var year = today.getFullYear();
 var est = new Date('Apr 16, 1996 15:45:55');
 var difference = today.getTime() - est.getTime();
 difference = (difference / 31556900000);
+
+var elMsg = document.getElementById('message');

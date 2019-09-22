7b63681b42ae9994d13639506ea9b1273138fa2b
CodeXRook <lacekitfly@gmail.com>
2019-09-22 06:20:41 -0400

    footer done

--- c03/js/document-object.js
+++ c03/js/document-object.js
@@ -3,3 +3,4 @@ msg += '<b>page address: </b>' + document.URL + '<br />';
 msg += '<b>last modified: </b>' + document.lastModified + '</p>';
 
 var el = document.getElementById('footer');
+el.innerHTML = msg;

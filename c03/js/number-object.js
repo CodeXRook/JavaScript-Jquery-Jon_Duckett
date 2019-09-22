735e0a30ee6497461ec40ed5428897d9b96343e8
CodeXRook <lacekitfly@gmail.com>
2019-09-22 07:06:56 -0400

    msg p tag

--- c03/js/number-object.js
+++ c03/js/number-object.js
@@ -2,3 +2,4 @@ var originalNumber = 10.23456;
 
 var msg = '<h2>original number</h2><p>' + originalNumber + '</p>';
 msg += '<h2>3 decimal places</h2><p>' + originalNumber.toFixed(3) + '</p>';
+msg += '<h2>3 digits</h2><p>' + originalNumber.toPrecision(3) + '</p>';

4e0d4c01d5cb5c62c6e857f5370bd1363763fc1c
CodeXRook <lacekitfly@gmail.com>
2019-09-21 21:31:26 -0400

    get new color with id

--- c02/js/update-array.js
+++ c02/js/update-array.js
@@ -2,6 +2,9 @@
 var colors = ['white',
               'black', 
               'cusotm']
-              
+
 //Update the third item in the array
 colors[2] = 'beige';
+
+//Get the element with an id of colors
+var el = document.getElementById('colors');

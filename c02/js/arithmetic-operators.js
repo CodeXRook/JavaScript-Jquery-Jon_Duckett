9cf96fda9181e7d319bf17c55236e080d478698b
CodeXRook <lacekitfly@gmail.com>
2019-09-21 20:29:21 -0400

    subtotal

--- c02/js/arithmetic-operators.js
+++ c02/js/arithmetic-operators.js
@@ -2,3 +2,5 @@ var subtotal = (13 + 1) *5 //Subtotal is 70
 var shipping = 0.5 * (13 + 1); // Shipping is 7
 
 var total = subtotal + shipping; // Total is 77
+
+var elSub = document.getElementById('subtotal');

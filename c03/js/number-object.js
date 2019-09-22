1229259c66f68f6746ff137f41679a5de4b9adee
CodeXRook <lacekitfly@gmail.com>
2019-09-22 07:07:48 -0400

    number obj done

--- c03/js/number-object.js
+++ c03/js/number-object.js
@@ -5,3 +5,4 @@ msg += '<h2>3 decimal places</h2><p>' + originalNumber.toFixed(3) + '</p>';
 msg += '<h2>3 digits</h2><p>' + originalNumber.toPrecision(3) + '</p>';
 
 var el = document.getElementById('info');
+el.innerHTML = msg;

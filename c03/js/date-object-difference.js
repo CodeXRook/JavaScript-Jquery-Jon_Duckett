567ed35c8d2a46eb45bf9d9d2e5c8a1e5f062d2e
CodeXRook <lacekitfly@gmail.com>
2019-09-22 06:15:27 -0400

    math floor

--- c03/js/date-object-difference.js
+++ c03/js/date-object-difference.js
@@ -5,3 +5,4 @@ var difference = today.getTime() - est.getTime();
 difference = (difference / 31556900000);
 
 var elMsg = document.getElementById('message');
+elMsg.textContent = Math.floor(difference) + ' years of online travel advice';

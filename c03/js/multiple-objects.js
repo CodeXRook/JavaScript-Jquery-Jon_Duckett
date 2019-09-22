2984dde0691355ef14791d50d3d67167f052447e
CodeXRook <lacekitfly@gmail.com>
2019-09-22 06:53:58 -0400

    elHotel

--- c03/js/multiple-objects.js
+++ c03/js/multiple-objects.js
@@ -11,3 +11,5 @@ var quayHotel = new Hotel('Quay', 40, 25);
 var parkHotel = new Hotel('Park', 120, 77);
 
 var details1 = quayHotel.name + ' rooms: ';
+  details1 += quayHotel.checkAvailability();
+var elHotel1 = document.getElementById('hotel1');

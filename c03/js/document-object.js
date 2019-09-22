c53d98b77607e54d733f83c06c271391187d76dc
CodeXRook <lacekitfly@gmail.com>
2019-09-22 06:19:55 -0400

    mod doc

--- c03/js/document-object.js
+++ c03/js/document-object.js
@@ -1,2 +1,3 @@
 var msg = '<p><b>page title: </b> ' + document.title + '<br />';
 msg += '<b>page address: </b>' + document.URL + '<br />';
+msg += '<b>last modified: </b>' + document.lastModified + '</p>';

e94293fe2df8c691f59dccd2fa3644ce08f1df5a
CodeXRook <lacekitfly@gmail.com>
2019-09-22 06:14:35 -0400

    differene time

--- c03/js/date-object-difference.js
+++ c03/js/date-object-difference.js
@@ -2,3 +2,4 @@ var today = new Date();
 var year = today.getFullYear();
 var est = new Date('Apr 16, 1996 15:45:55');
 var difference = today.getTime() - est.getTime();
+difference = (difference / 31556900000);

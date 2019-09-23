0362f6854690c24dd50ea07d4f80d6a114559db4
CodeXRook <lacekitfly@gmail.com>
2019-09-23 13:28:58 -0400

    msg screen width

--- c03/js/window-object.js
+++ c03/js/window-object.js
@@ -2,3 +2,4 @@ var msg = '<h2>browser window</h2><p>width: ' + window.innerWidth + '</p>';
 msg += '<p>height: ' + window.innerHeight + '</p>';
 
 msg += '<h2>history</h2> <p>items: ' + window.history.length + '</p>';
+msg += '<h2>screen</h2> <p> width: ' + window.screen.width + '</p>';

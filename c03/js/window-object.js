837bbd0c55cdd367a64a8d913d05ca9c60eb3503
CodeXRook <lacekitfly@gmail.com>
2019-09-23 13:28:30 -0400

    msg history length

--- c03/js/window-object.js
+++ c03/js/window-object.js
@@ -1,2 +1,4 @@
 var msg = '<h2>browser window</h2><p>width: ' + window.innerWidth + '</p>';
 msg += '<p>height: ' + window.innerHeight + '</p>';
+
+msg += '<h2>history</h2> <p>items: ' + window.history.length + '</p>';

f820fd253a0e8515773e4cdbbe6ed918a579e0bd
CodeXRook <lacekitfly@gmail.com>
2019-09-23 13:21:35 -0400

    first ee

--- c03/js/string-object.js
+++ c03/js/string-object.js
@@ -5,3 +5,4 @@ msg +='<h2>uppercase</h2><p>' + saying.length +'</p>';
 msg += '<h2>lowercase</h2><p>' + saying.toUpperCase() + '</p>';
 
 msg += '<h2>character index: 12</h2><p>' + saying.charAt(12) + '</p>';
+msg += '<h2>fisrt ee</h2><p>' + saying.indexOf('ee') + '</p>';

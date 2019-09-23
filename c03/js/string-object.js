d17f48fb0f6c0c681baf368dcabf4fd7af69a456
CodeXRook <lacekitfly@gmail.com>
2019-09-23 13:20:20 -0400

    new variable

--- c03/js/string-object.js
+++ c03/js/string-object.js
@@ -2,3 +2,4 @@ var saying = 'Home sweet home ' ;
 var msg = '<h2>length</h2><p>' + saying.length + '</p>';
 
 msg +='<h2>uppercase</h2><p>' + saying.length +'</p>';
+msg += '<h2>lowercase</h2><p>' + saying.toUpperCase() + '</p>';

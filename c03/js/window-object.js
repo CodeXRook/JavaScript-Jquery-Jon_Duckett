58ba1dd8ff6cf10eb7f52f75ad93ade7b5861b38
CodeXRook <lacekitfly@gmail.com>
2019-09-23 13:29:30 -0400

    msg screen height

--- c03/js/window-object.js
+++ c03/js/window-object.js
@@ -3,3 +3,4 @@ msg += '<p>height: ' + window.innerHeight + '</p>';
 
 msg += '<h2>history</h2> <p>items: ' + window.history.length + '</p>';
 msg += '<h2>screen</h2> <p> width: ' + window.screen.width + '</p>';
+msg += '<p> height: ' + window.screen.height + '</p>';

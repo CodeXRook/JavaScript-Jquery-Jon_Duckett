72da413fc2e160952539b0431ffbfb141f8587bc
CodeXRook <lacekitfly@gmail.com>
2019-09-25 16:50:02 -0400

    if operator ===

--- c04/js/example.js
+++ c04/js/example.js
@@ -2,3 +2,7 @@ var table = 3;             // Unit of table
 var operator = 'addition'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '';              // Message
+
+if (operator === 'addition'){
+    
+}

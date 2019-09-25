3b89fc0ee57c0249b72c5404b63a20055346c023
CodeXRook <lacekitfly@gmail.com>
2019-09-24 23:47:06 -0400

    var passBoth

--- c04/js/logical-and.js
+++ c04/js/logical-and.js
@@ -4,3 +4,4 @@ var pass1 = 6; //Round 1 pass mark
 var pass2 = 6; //Round 2 pass mark
 
 // Check whether user passed both rounds. store results in variable
+var passBoth = (score1 >= pass1) && (score2 >= pass2);

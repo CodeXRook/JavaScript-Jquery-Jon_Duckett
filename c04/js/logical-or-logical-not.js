cf7c799b37dd14e2f677b5694d984f4752552579
CodeXRook <lacekitfly@gmail.com>
2019-09-24 23:51:46 -0400

    check whether user passed two rounds

--- c04/js/logical-or-logical-not.js
+++ c04/js/logical-or-logical-not.js
@@ -2,3 +2,5 @@ var score1 = 8; // Round 1 score
 var score2 = 8; // Round 2 score
 var pass1 = 6; // Round 1 pass mark
 var pass2 = 6; // Round 2 pass mark
+
+// Check whether user passed one of the two rounds, store result in variable

a74ebb05cd32a388966f630e21d794af392687a0
CodeXRook <lacekitfly@gmail.com>
2019-09-25 16:55:44 -0400

    var el

--- c04/js/example.js
+++ c04/js/example.js
@@ -5,13 +5,17 @@ var msg = '';              // Message
 
 if (operator === 'addition'){
      // Do addition 
-     while (i < 11) {
+     while (i < 11){
       msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
       i++;    
-     }
-}  else {
+  }
+} else {
     // Do multiplication
     while (i < 11) {
-        
+       msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+       i++; 
     }
-}
+} 
+
+// Write the message into the page
+var el = document.getElementById('blackboard');

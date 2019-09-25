b7b47c7b02aaf1ddabbaf4688e0e72b4c5e95407
CodeXRook <lacekitfly@gmail.com>
2019-09-24 23:20:20 -0400

    check is score higher than current

--- c04/js/comparison-operator-continued.js
+++ c04/js/comparison-operator-continued.js
@@ -2,3 +2,5 @@ var score1 = 90; //Round 1 score
 var score2 = 95; //Round 2 score
 var highScore1 = 75; // Round 1 high score
 var highScore2 = 95; //Round 2 high score
+
+// Check if score are higher than current high score

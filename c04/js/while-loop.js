19f9b710fc55c5ab4f5ce7a94bcfaf383f934b79
CodeXRook <lacekitfly@gmail.com>
2019-09-25 16:32:09 -0400

    msg plus one

--- c04/js/while-loop.js
+++ c04/js/while-loop.js
@@ -2,4 +2,6 @@ var i = 1;  // Set counter to 1
 var msg = '';    // Message
 
 // Store 5 times table in a variable
-while (i < 10)
+while (i < 10) {
+    msg += i + ' x 5 = ' + (i * 5) + '<br />';
+}

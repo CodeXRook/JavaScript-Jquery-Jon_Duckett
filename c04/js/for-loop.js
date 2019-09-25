284ed2a022fe1fb544dff773a187095a184eb930
CodeXRook <lacekitfly@gmail.com>
2019-09-25 16:23:34 -0400

    var msg

--- c04/js/for-loop.js
+++ c04/js/for-loop.js
@@ -1,3 +1,5 @@
 var scores = [24, 32, 17];  // Array of scores
 var arrayLength = scores.length;// Items in array
 var roundNumber = 0;            // Current round
+var msg = '';                   // Message
+

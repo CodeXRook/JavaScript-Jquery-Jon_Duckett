1d24a64c49d512d1730bd4626b2ba819ff753edc
CodeXRook <lacekitfly@gmail.com>
2019-09-24 23:21:14 -0400

    write msg into page

--- c04/js/comparison-operator-continued.js
+++ c04/js/comparison-operator-continued.js
@@ -4,3 +4,6 @@ var highScore1 = 75; // Round 1 high score
 var highScore2 = 95; //Round 2 high score
 
 // Check if score are higher than current high score
+var comparison = (score1 + score2) > (highScore1 + highScore2);
+
+// Write the message into the page

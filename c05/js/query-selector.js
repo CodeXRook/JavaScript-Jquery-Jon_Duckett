b88802eb5bb0681c09db92a22dad073c888154a7
CodeXRook <lacekitfly@gmail.com>
2019-09-26 02:13:11 -0400

    className

--- c05/js/query-selector.js
+++ c05/js/query-selector.js
@@ -5,3 +5,4 @@ el.className = 'cool';
 // querySelectorAll returns a NodeList.
 // The third li element is then selected and changed.
 var els = document.querySelectorAll('li.hot');
+els[1].className = 'cool';

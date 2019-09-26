8e7555e4a9c659ad5ae76838f7eb4676c9e48a9e
CodeXRook <lacekitfly@gmail.com>
2019-09-26 01:39:21 -0400

    change value

--- c05/js/get-element-by-id.js
+++ c05/js/get-element-by-id.js
@@ -1,2 +1,4 @@
 // Select the element and store it in a variable.  
-var el = document.getElementById('one');         
+var el = document.getElementById('one');       
+
+// Change the value of the class attribute.

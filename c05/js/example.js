b43fa80d6e8a9917495bc3c3356bd19ed1abb9e3
CodeXRook <lacekitfly@gmail.com>
2019-09-27 01:57:09 -0400

    textNode

--- c05/js/example.js
+++ c05/js/example.js
@@ -3,4 +3,4 @@ var list = document.getElementsByTagName('ul')[0];   // Get the <ul> element
 
 // ADD NEW ITEM TO END OF LIST
 var newItemLast = document.createElement('li');    // Create element
-var newTextLast
+var newTextLast = document.createTextNode('cream'); 

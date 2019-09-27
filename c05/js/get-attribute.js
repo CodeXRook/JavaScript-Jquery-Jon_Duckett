0523b039945a18ed0e7b07670977af5bea66fd52
CodeXRook <lacekitfly@gmail.com>
2019-09-27 01:35:47 -0400

    get attr clas

--- c05/js/get-attribute.js
+++ c05/js/get-attribute.js
@@ -1,5 +1,7 @@
 var firstItem =document.getElementById('one')//Get first list item 
  if(firstItem.hasAttribute('class')) {    //If it has class attribute
-    var attr
+    var attr = firstItem.getAttribute('class') //Get the attribute
+
+    
 
  }

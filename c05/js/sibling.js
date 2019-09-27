d89963317a07029d2eab2966fc4b950d82ab8152
CodeXRook <lacekitfly@gmail.com>
2019-09-26 23:18:53 -0400

    Change values

--- c05/js/sibling.js
+++ c05/js/sibling.js
@@ -2,3 +2,5 @@
 var startItem = document.getElementById('two');
 var prevItem = startItem.previousSibling;
 var nextItem = startItem.nextSibling;
+
+// Change the values of the siblings' class attributes.

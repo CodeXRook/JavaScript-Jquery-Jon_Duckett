55cd773ba4579c4d3664090a33f91b1cb08aef59
CodeXRook <lacekitfly@gmail.com>
2019-09-27 01:34:32 -0400

    new attr set

--- c05/js/get-attribute.js
+++ c05/js/get-attribute.js
@@ -1,4 +1,5 @@
 var firstItem =document.getElementById('one')//Get first list item 
- if(firstItem.hasAttribute('class')) {
-     
+ if(firstItem.hasAttribute('class')) {    //If it has class attribute
+    var attr
+
  }

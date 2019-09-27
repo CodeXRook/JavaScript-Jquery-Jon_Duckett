ddfe43d73e2c2a194504218092a5377eee831418
CodeXRook <lacekitfly@gmail.com>
2019-09-27 00:00:28 -0400

    Get value

--- c05/js/inner-text-and-text-content.js
+++ c05/js/inner-text-and-text-content.js
@@ -1,2 +1,3 @@
 var firstItem = document.getElementById('one'); // Find first list item
 var showTextContent = firstItem.textContent;  // Get value of textContent
+var showInnerText = firstItem.innerText;  // Get value of innerText

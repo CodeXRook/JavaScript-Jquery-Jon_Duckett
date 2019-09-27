fa4363017e132148162141052116edbe4d19d1b1
CodeXRook <lacekitfly@gmail.com>
2019-09-27 01:40:56 -0400

    set attribute

--- c05/js/get-attribute.js
+++ c05/js/get-attribute.js
@@ -5,6 +5,4 @@ var firstItem =document.getElementById('one')//Get first list item
     // Add the value of the attribute after the list
     var el = document.getElementById('scriptResults');
     el.innerHTML = '<p>The first item has a class name: ' + attr + '</p>';
-    
-
  }

4f1ffb8d209b3ef3fd0b6df4a0b7e00552e000c5
CodeXRook <lacekitfly@gmail.com>
2019-09-27 00:18:47 -0400

    var msg

--- c05/js/inner-text-and-text-content.js
+++ c05/js/inner-text-and-text-content.js
@@ -3,3 +3,4 @@ var showTextContent = firstItem.textContent;  // Get value of textContent
 var showInnerText = firstItem.innerText;  // Get value of innerText
 
 // Show the content of these two properties at the end of the list
+var msg

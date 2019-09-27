2165701be7055410af23885478190d368d8a1d13
CodeXRook <lacekitfly@gmail.com>
2019-09-27 01:50:56 -0400

    If it has a class

--- c05/js/remove-attribute.js
+++ c05/js/remove-attribute.js
@@ -1,2 +1,4 @@
 var firstItem = document.getElementById('one'); // Get the first item
-if (firstItem.hasAttribute('class'))
+if (firstItem.hasAttribute('class')) {  // If it has a class attribute
+
+}

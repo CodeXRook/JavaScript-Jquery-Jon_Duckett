9a6fcb286642e06de1a986b387c2e9545289a056
CodeXRook <lacekitfly@gmail.com>
2019-09-27 00:19:25 -0400

    var showtext

--- c05/js/inner-text-and-text-content.js
+++ c05/js/inner-text-and-text-content.js
@@ -3,4 +3,4 @@ var showTextContent = firstItem.textContent;  // Get value of textContent
 var showInnerText = firstItem.innerText;  // Get value of innerText
 
 // Show the content of these two properties at the end of the list
-var msg
+var msg = '<p>textContent: ' + showTextContent + '</p>';

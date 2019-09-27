b7f5bfa881327867fb9bf79b3df4ad4f0d2af7ae
CodeXRook <lacekitfly@gmail.com>
2019-09-26 23:52:18 -0400

    elText.replace

--- c05/js/node-value.js
+++ c05/js/node-value.js
@@ -1,3 +1,3 @@
 var itemTwo = document.getElementById('two');  // Get second list item
 var elText = itemTwo.firstChild.nodeValue; // Get its text content
-elText 
+elText = elText.replace('pine nuts', 'kale');  // Change pine nuts to kale

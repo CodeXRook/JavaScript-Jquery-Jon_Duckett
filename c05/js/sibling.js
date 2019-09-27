ab449e3c72507542cf0bceafd23451e8a319ac19
CodeXRook <lacekitfly@gmail.com>
2019-09-26 23:18:21 -0400

    nextItem

--- c05/js/sibling.js
+++ c05/js/sibling.js
@@ -1,4 +1,4 @@
 //  Select the starting point and find its siblings
 var startItem = document.getElementById('two');
 var prevItem = startItem.previousSibling;
-var nextItem
+var nextItem = startItem.nextSibling;

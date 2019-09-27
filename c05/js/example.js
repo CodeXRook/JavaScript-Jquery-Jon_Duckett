e2d3cc95292620fb3d37dfbdb74b14741a2d2d34
CodeXRook <lacekitfly@gmail.com>
2019-09-27 02:05:19 -0400

    Add element to list

--- c05/js/example.js
+++ c05/js/example.js
@@ -11,4 +11,4 @@ list.appendChild(newItemLast);   // Add element end of list
 var newItemFirst = document.createElement('li');  // Create element
 var newTextFirst = document.createTextNode('kale'); // Create text node
 newItemFirst.appendChild(newTextFirst); // Add text node to element
-list.insertBefore(newItemFirst, list.firstChild);
+list.insertBefore(newItemFirst, list.firstChild);  // Add element to list

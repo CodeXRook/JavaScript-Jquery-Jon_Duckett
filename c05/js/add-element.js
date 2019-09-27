60d971e0bbe78b30db65a0cfe8b4795dbf36b35d
CodeXRook <lacekitfly@gmail.com>
2019-09-27 00:39:55 -0400

    find the pos

--- c05/js/add-element.js
+++ c05/js/add-element.js
@@ -6,3 +6,5 @@ var newText = document.createTextNode('quinoa');
 
 // Attach the new text node to the new element.
 newEl.appendChild(newText);
+
+// Find the position where the new element should be added.

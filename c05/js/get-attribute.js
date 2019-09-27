b1f495bd9296b2f9c4ea1be0d43a080b1b4c0846
CodeXRook <lacekitfly@gmail.com>
2019-09-27 01:38:34 -0400

    innerHtml set attr

--- c05/js/get-attribute.js
+++ c05/js/get-attribute.js
@@ -4,6 +4,7 @@ var firstItem =document.getElementById('one')//Get first list item
 
     // Add the value of the attribute after the list
     var el = document.getElementById('scriptResults');
-    el.innerHTML
+    el.innerHTML = '<p>The first item has a class name: ' + attr + '</p>';
+    
 
  }

d4b021ef2f9e08b6064d217a4d3a4bd1f69ae062
CodeXRook <lacekitfly@gmail.com>
2019-09-27 00:18:22 -0400

    Show the content of 2 prop

--- c05/js/inner-text-and-text-content.js
+++ c05/js/inner-text-and-text-content.js
@@ -1,3 +1,5 @@
 var firstItem = document.getElementById('one'); // Find first list item
 var showTextContent = firstItem.textContent;  // Get value of textContent
 var showInnerText = firstItem.innerText;  // Get value of innerText
+
+// Show the content of these two properties at the end of the list

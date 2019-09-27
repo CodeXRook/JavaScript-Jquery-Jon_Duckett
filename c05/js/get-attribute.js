67fb85b10eb1a77239c42b9c2c6f935453ca8f7d
CodeXRook <lacekitfly@gmail.com>
2019-09-27 01:36:40 -0400

    add the value of the attribute after the list

--- c05/js/get-attribute.js
+++ c05/js/get-attribute.js
@@ -2,6 +2,6 @@ var firstItem =document.getElementById('one')//Get first list item
  if(firstItem.hasAttribute('class')) {    //If it has class attribute
     var attr = firstItem.getAttribute('class') //Get the attribute
 
-    
+    // Add the value of the attribute after the list
 
  }

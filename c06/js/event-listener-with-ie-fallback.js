d66e4746798c154fe8cfa793093a795243794234
CodeXRook <lacekitfly@gmail.com>
2019-09-28 06:01:32 -0400

    if statement

--- c06/js/event-listener-with-ie-fallback.js
+++ c06/js/event-listener-with-ie-fallback.js
@@ -2,5 +2,6 @@ var elUsername = document.getElementById('username'); // Get username input
 var elMsg = document.getElementById('feedback'); // Get feedback element
 
 function checkUsername(minLength) {              // Declare funcion
+    if ()
 
 }

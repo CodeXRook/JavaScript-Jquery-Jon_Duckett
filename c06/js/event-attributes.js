efaff57f0d74490b62b1ca57b252be061cffef51
CodeXRook <lacekitfly@gmail.com>
2019-09-28 04:27:21 -0400

    else statement

--- c06/js/event-attributes.js
+++ c06/js/event-attributes.js
@@ -2,6 +2,8 @@ function checkUsername() { // Declar function
     var elMsg = document.getElementById('feddback'); //Get feedback element
     var elUsername = document.getElementById('username'); // Get username input
     if(elUsername.nodeValue.lenght<5) {                   // If username too short
-        elMsg.textContent = ''
+        elMsg.textContent = 'Username must be 5 characters or more'; // Set msg
+    }else {
+        
     }
 }

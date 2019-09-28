f1b3358c71370ae6aa2fee15e39ea19b45efeb75
CodeXRook <lacekitfly@gmail.com>
2019-09-28 06:02:44 -0400

    if username too short

--- c06/js/event-listener-with-ie-fallback.js
+++ c06/js/event-listener-with-ie-fallback.js
@@ -2,6 +2,8 @@ var elUsername = document.getElementById('username'); // Get username input
 var elMsg = document.getElementById('feedback'); // Get feedback element
 
 function checkUsername(minLength) {              // Declare funcion
-    if (elUsername.nodeValue.length < minLength)
+    if (elUsername.nodeValue.length < minLength) { // If username too short
+
+    }
 
 }

c63ba72a5d78bbcbf0d0b323b783340e3a495dfe
CodeXRook <lacekitfly@gmail.com>
2019-09-28 06:12:16 -0400

    else statement

--- c06/js/event-listener-with-ie-fallback.js
+++ c06/js/event-listener-with-ie-fallback.js
@@ -11,9 +11,8 @@ function checkUsername(minLength) {              // Declare funcion
 }
 if(elUsername.addEventListener) {                  // If event listener supported
     elUsername.addEventListener('blur', function(){// When username loses focus
-        checkUsername(5);                         // Call checkUsername()
-        , false);                                // Capture during bubble phase
-
-    }
-
-}
+      checkUsername(5);                         // Call checkUsername()
+    }, false);                                // Capture during bubble phase
+ }else {
+     
+ }

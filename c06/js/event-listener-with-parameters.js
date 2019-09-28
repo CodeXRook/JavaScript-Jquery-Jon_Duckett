713e53c41a4df612d09b4352a50c878a803d0965
CodeXRook <lacekitfly@gmail.com>
2019-09-28 05:36:41 -0400

    innerHtml

--- c06/js/event-listener-with-parameters.js
+++ c06/js/event-listener-with-parameters.js
@@ -2,6 +2,9 @@ var elUsername = document.getElementById('username');// Username input
 var elMsg   = document.getElementById('feedback');// Error msg element
 
 function checkUsername(minLength)  {              // Declare function
-    if()
+    if(elUsername.value.length < minLength){      // If username too short 
+    // Set the error message
+    elMsg.innerHTML
+    }
 
 }

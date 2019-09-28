695a551917035902486b017a2622a6667a21e8f6
CodeXRook <lacekitfly@gmail.com>
2019-09-28 05:32:30 -0400

    miniLength

--- c06/js/event-listener-with-parameters.js
+++ c06/js/event-listener-with-parameters.js
@@ -1,4 +1,6 @@
 var elUsername = document.getElementById('username');// Username input
 var elMsg   = document.getElementById('feedback');// Error msg element
 
-function checkUsername
+function checkUsername(minLength)  {
+    
+}

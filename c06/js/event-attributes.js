b35c99b37326bb3a888608278daede4370c91d2b
CodeXRook <lacekitfly@gmail.com>
2019-09-28 04:23:19 -0400

    if username too short

--- c06/js/event-attributes.js
+++ c06/js/event-attributes.js
@@ -1,7 +1,7 @@
 function checkUsername() { // Declar function
     var elMsg = document.getElementById('feddback'); //Get feedback element
     var elUsername = document.getElementById('username'); // Get username input
-    if(elUsername.nodeValue.lenght<5) {
-        
+    if(elUsername.nodeValue.lenght<5) {                   // If username too short
+
     }
 }

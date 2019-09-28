33a7e489e638a0f4b1208c43126fd499871aa0f9
CodeXRook <lacekitfly@gmail.com>
2019-09-28 05:07:59 -0400

    checkuserName

--- c06/js/event-handler.js
+++ c06/js/event-handler.js
@@ -9,4 +9,4 @@ function checkUsername(){           // Declare function
 }
 
 var elUsername = document.getElementById('username'); // Get username input
-elUsername.onblur
+elUsername.onblur = checkUsername;

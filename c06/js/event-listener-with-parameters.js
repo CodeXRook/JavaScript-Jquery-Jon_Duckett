c0d2ce1e1034c99c7352b856f34b79befb03eda0
CodeXRook <lacekitfly@gmail.com>
2019-09-28 05:34:37 -0400

    if statement

--- c06/js/event-listener-with-parameters.js
+++ c06/js/event-listener-with-parameters.js
@@ -1,6 +1,7 @@
 var elUsername = document.getElementById('username');// Username input
 var elMsg   = document.getElementById('feedback');// Error msg element
 
-function checkUsername(minLength)  {
-    
+function checkUsername(minLength)  {              // Declare function
+    if()
+
 }

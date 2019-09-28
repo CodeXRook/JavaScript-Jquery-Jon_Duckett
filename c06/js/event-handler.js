2d53a39ac3684de3f8cabe448561573183c0e9b4
CodeXRook <lacekitfly@gmail.com>
2019-09-28 04:59:12 -0400

    set msg

--- c06/js/event-handler.js
+++ c06/js/event-handler.js
@@ -1,6 +1,6 @@
 function checkUsername(){           // Declare function
   var elMsg = document.getElementById('feedback'); //Get feedback element
   if(this.value.length < 5) {                       // If username too short
-    elMsg.textContent 
+    elMsg.textContent = 'Username must be 5 characters or more'; 
   }
 }

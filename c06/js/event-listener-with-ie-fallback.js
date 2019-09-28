f6db8fc469b92b7e8947d1d6b70a2b2a31c0b98c
CodeXRook <lacekitfly@gmail.com>
2019-09-28 06:02:11 -0400

    min length

--- c06/js/event-listener-with-ie-fallback.js
+++ c06/js/event-listener-with-ie-fallback.js
@@ -2,6 +2,6 @@ var elUsername = document.getElementById('username'); // Get username input
 var elMsg = document.getElementById('feedback'); // Get feedback element
 
 function checkUsername(minLength) {              // Declare funcion
-    if ()
+    if (elUsername.nodeValue.length < minLength)
 
 }

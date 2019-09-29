c828c916057d9740223ab82f00fabda0c19706e5
CodeXRook <lacekitfly@gmail.com>
2019-09-28 23:35:52 -0400

    value.lenght < minLen

--- c06/js/event-listener-with-event-object.js
+++ c06/js/event-listener-with-event-object.js
@@ -6,4 +6,6 @@ e = window.event;                         // Use IE fallback
 el = e.target || e.srcElement;            // Get target of event
 elMsg = el.nextSibling;                    // Get its next sibling
 
-if 
+if (el.value.length < minLength) {
+    
+}

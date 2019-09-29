0cf1779e991a02653e6872d145be8a5a95c00eb3
CodeXRook <lacekitfly@gmail.com>
2019-09-28 23:57:13 -0400

    username set

--- c06/js/event-listener-with-event-object.js
+++ c06/js/event-listener-with-event-object.js
@@ -14,6 +14,6 @@ if (el.value.length < minLength) {          // If length is too short set msg
 }
 
 var elUsername  = document.getElementById('username'); // Get username input
-if(){
-    
+if(elUsername.addEventListener){
+
 }

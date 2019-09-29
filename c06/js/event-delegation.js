c47a1033c2bf2e87bda9c62d48aa7d25d3dff51a
CodeXRook <lacekitfly@gmail.com>
2019-09-29 01:25:10 -0400

    Get the target

--- c06/js/event-delegation.js
+++ c06/js/event-delegation.js
@@ -2,4 +2,5 @@ function getTarget(e) {                      // Declare function
     if (!e) {                                 // If there is no event object
         e = window.event;                     // Use old IE event object
     }
+    return e.target || e.srcElement;          // Get the target of event
 }

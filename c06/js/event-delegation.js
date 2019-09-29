b716778ed4dbf04dcb78dbb87d2b769a6bef4bd2
CodeXRook <lacekitfly@gmail.com>
2019-09-29 01:24:30 -0400

    Use old IE

--- c06/js/event-delegation.js
+++ c06/js/event-delegation.js
@@ -1,5 +1,5 @@
 function getTarget(e) {                      // Declare function
     if (!e) {                                 // If there is no event object
-
+        e = window.event;                     // Use old IE event object
     }
 }

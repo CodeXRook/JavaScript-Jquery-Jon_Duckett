7d6f032d9d62f41a888f272798525fa8d4c449d9
CodeXRook <lacekitfly@gmail.com>
2019-09-29 01:56:31 -0400

    need event function

--- c06/js/event-delegation.js
+++ c06/js/event-delegation.js
@@ -29,6 +29,7 @@ function itemDone(e) {                           // Declare function
 }
 // Set up event listeners to call itemDone() on click
 var el = document.getElementById('shoppingList');  // Get shopping list
-if(el.addEventListener){
-    
+if(el.addEventListener){                             // If event listeners work
+    el.addEventListener
+
 }

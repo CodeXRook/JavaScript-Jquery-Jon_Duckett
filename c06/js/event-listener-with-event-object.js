ccd7af1ec6330744f3c0fb8ff3eda781d59065f0
CodeXRook <lacekitfly@gmail.com>
2019-09-28 23:33:57 -0400

    get target of event

--- c06/js/event-listener-with-event-object.js
+++ c06/js/event-listener-with-event-object.js
@@ -3,4 +3,5 @@ var el, elMsg;                         // Declare variables
 if (!e){                                // If event object doesn't exist
 e = window.event;                         // Use IE fallback
 }
-el = e.target || e.srcElement;
+el = e.target || e.srcElement;            // Get target of event
+ 

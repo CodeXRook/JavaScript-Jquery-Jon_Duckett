c865638006127d6f93e20696c1f5b843dc0a6fb6
CodeXRook <lacekitfly@gmail.com>
2019-09-29 01:47:40 -0400

    prevent link from taking you else where

--- c06/js/event-delegation.js
+++ c06/js/event-delegation.js
@@ -18,7 +18,7 @@ function itemDone(e) {                           // Declare function
     if(target.nodeName.toLowerCase() == "em" ) { // If the user clicked on an em element
         elListItem = target.parentNode.parentNode;  // Get its li element
         elList = elListItem.parentNode;            // Get the ul element
-        elList.removeChild(elListItem);     
-
+        elList.removeChild(elListItem);            // Remove list item from list
     }
+    // Prevent the link from taking you elsewhere
 }

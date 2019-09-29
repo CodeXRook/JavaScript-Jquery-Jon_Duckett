5e59e1b3dc7b5a48ec77c4e634277e89918aa35a
CodeXRook <lacekitfly@gmail.com>
2019-09-29 00:00:06 -0400

    checkLength

--- c06/js/event-listener-with-event-object.js
+++ c06/js/event-listener-with-event-object.js
@@ -16,6 +16,8 @@ if (el.value.length < minLength) {          // If length is too short set msg
 var elUsername  = document.getElementById('username'); // Get username input
 if(elUsername.addEventListener){                       // If event listener supported
   elUsername.addEventListener('blur', function(e) {   // On blur event
+    // NOTE: This function is checkLength() - not checkUsername()
+    checkLength(e, 5); 
 
   }
 }

aa8886bbaf1ca2a3fd390fceb1fa37bd6fae0fe6
CodeXRook <lacekitfly@gmail.com>
2019-09-30 02:17:58 -0400

    Num of chars left

--- c06/js/keypress.js
+++ c06/js/keypress.js
@@ -4,5 +4,5 @@ function charCount(e){                 // Declare function
   var textEntered, charDisplay, counter, lastkey;   // Declare variables
   textEntered = document.getElementById('message').value;  // User's text
   charDisplay = document.getElementById('charactersLeft');  // Counter element
-  counter  = (180 - (textEntered.length));  
+  counter  = (180 - (textEntered.length));                    // Num of chars left
 }

24a634ff08e676c9a43d00f791e9e4eb1aaaf960
CodeXRook <lacekitfly@gmail.com>
2019-09-30 18:56:34 -0400

    function

--- c06/js/mutation.js
+++ c06/js/mutation.js
@@ -3,3 +3,5 @@ var elList, addLink, newEl, newText, counter, listItems;  // Declare variables
 elList = document.getElementById('list');                  // Get list
 addLink = document.querySelector('a');                     // Get add item button
 counter = document.getElementById('counter');              // Get item counter
+
+function

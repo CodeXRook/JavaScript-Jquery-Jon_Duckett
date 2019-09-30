4c2b59ce01bfb9269ab8e62221627e365eeba040
CodeXRook <lacekitfly@gmail.com>
2019-09-30 19:35:07 -0400

    return MSG

--- c06/js/html5-events.js
+++ c06/js/html5-events.js
@@ -9,4 +9,5 @@ window.addEventListener('beforeunload', function(event) {
     // This example has been updated to improve cross-browser compatibility (as recommended by MDN)
     var  message = 'You have changes that have not been saved';
     (event || window.event).returnValue = message;
+     return message;
 }

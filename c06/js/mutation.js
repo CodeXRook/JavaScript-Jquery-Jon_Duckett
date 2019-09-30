0765a159daedd639268cbf5b3e3b63a9163dd4c0
CodeXRook <lacekitfly@gmail.com>
2019-09-30 19:16:04 -0400

    newText

--- c06/js/mutation.js
+++ c06/js/mutation.js
@@ -7,5 +7,6 @@ counter = document.getElementById('counter');              // Get item counter
 function addItem(e){                                       // Declare function
     e.preventDefault();                                    // Prevent link action
     newEl = document.createElement('li');                  // New <li> element
-    newText = document.createTextNode('New list item');
+    newText = document.createTextNode('New list item');    // New text node
+    newEl.appendChild(newText); 
 }

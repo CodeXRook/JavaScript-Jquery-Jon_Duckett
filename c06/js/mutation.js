880776ea33f7edc706d709bf0d85d176c6f7c28d
CodeXRook <lacekitfly@gmail.com>
2019-09-30 18:58:31 -0400

    new

--- c06/js/mutation.js
+++ c06/js/mutation.js
@@ -6,4 +6,5 @@ counter = document.getElementById('counter');              // Get item counter
 
 function addItem(e){                                       // Declare function
     e.preventDefault();                                    // Prevent link action
+    newEl
 }

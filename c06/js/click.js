eb5436c0ce8fc36cd27b17de40e6ceac67dbc114
CodeXRook <lacekitfly@gmail.com>
2019-09-29 23:57:58 -0400

    new msg:

--- c06/js/click.js
+++ c06/js/click.js
@@ -2,3 +2,4 @@
 var msg = '<div class=\"header\"><a id=\"close\" href="#">close X</a></div>';
 msg += '<div><h2>System Maintenance</h2>';
 msg += 'Our servers are being updated between 3 and 4 a.m. ';
+msg +=

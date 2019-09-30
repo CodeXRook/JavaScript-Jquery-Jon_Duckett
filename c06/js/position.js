0f354b0adf506e2856fb34a40a0e078907bd3615
CodeXRook <lacekitfly@gmail.com>
2019-09-30 01:05:17 -0400

    var py

--- c06/js/position.js
+++ c06/js/position.js
@@ -1,2 +1,4 @@
-var sx = document.getElementById('sx');         // Element to hold screenX
-var sy = document.getElementById
+var sx = document.getElementById('sx');            // Element to hold screenX
+var sy = document.getElementById('sy');            // Element to hold screenY
+var px = document.getElementById('px');            // Element to hold pageX
+var py = document.getElementById('py');            // 

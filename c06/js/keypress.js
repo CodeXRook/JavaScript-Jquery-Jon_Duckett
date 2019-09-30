075c3cd98074b77c826fa62672cfb127e2a7da08
CodeXRook <lacekitfly@gmail.com>
2019-09-30 02:17:29 -0400

    counter set

--- c06/js/keypress.js
+++ c06/js/keypress.js
@@ -4,5 +4,5 @@ function charCount(e){                 // Declare function
   var textEntered, charDisplay, counter, lastkey;   // Declare variables
   textEntered = document.getElementById('message').value;  // User's text
   charDisplay = document.getElementById('charactersLeft');  // Counter element
-  counter
+  counter  = (180 - (textEntered.length));  
 }

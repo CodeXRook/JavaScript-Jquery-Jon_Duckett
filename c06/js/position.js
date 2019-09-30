4e6ec26faf42a97edeaefac53a2e7577a4501a9c
CodeXRook <lacekitfly@gmail.com>
2019-09-30 01:10:40 -0400

    cy

--- c06/js/position.js
+++ c06/js/position.js
@@ -3,3 +3,4 @@ var sy = document.getElementById('sy');            // Element to hold screenY
 var px = document.getElementById('px');            // Element to hold pageX
 var py = document.getElementById('py');            // Element to hold pageY
 var cx = document.getElementById('cx');           // Element to hold clientX
+var cy  = document.getElementById('cy');

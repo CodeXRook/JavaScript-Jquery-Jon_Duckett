5f8040ebe12107b050a2e8bf7623b71c6a4f8cb4
CodeXRook <lacekitfly@gmail.com>
2019-09-30 02:22:25 -0400

    new var

--- c06/js/keypress.js
+++ c06/js/keypress.js
@@ -9,3 +9,4 @@ function charCount(e){                 // Declare function
   lastkey = document.getElementById('lastKey');              // Get last key elem
   lastkey.textContent = 'Last key in ASCII code: ' + e.keyCode; // Create msg 
 }
+el

ed0acd352b3694f7939bedafd9f0f920e32c5616
CodeXRook <lacekitfly@gmail.com>
2019-09-30 02:21:04 -0400

    get last key elem

--- c06/js/keypress.js
+++ c06/js/keypress.js
@@ -6,5 +6,6 @@ function charCount(e){                 // Declare function
   charDisplay = document.getElementById('charactersLeft');  // Counter element
   counter  = (180 - (textEntered.length));                    // Num of chars left
   charDisplay.textContent = counter;                         // Show chars left
-  lastkey = document.getElementById('lastKey');  
+  lastkey = document.getElementById('lastKey');              // Get last key elem
+ 
 }

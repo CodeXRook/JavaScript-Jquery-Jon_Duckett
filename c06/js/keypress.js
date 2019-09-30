84609de24edb08414d9ac34210e039463ba03263
CodeXRook <lacekitfly@gmail.com>
2019-09-30 02:15:06 -0400

    user text

--- c06/js/keypress.js
+++ c06/js/keypress.js
@@ -2,5 +2,5 @@ var el;                               // Declare variables
 
 function charCount(e){                 // Declare function
   var textEntered, charDisplay, counter, lastkey;   // Declare variables
-  textEntered = document.getElementById
+  textEntered = document.getElementById('message').value;  // User's text
 }

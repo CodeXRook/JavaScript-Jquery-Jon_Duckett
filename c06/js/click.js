3a4b6380b987631a1f01e6df8745aaf87c1cc5c0
CodeXRook <lacekitfly@gmail.com>
2019-09-30 00:01:40 -0400

    Add an Id

--- c06/js/click.js
+++ c06/js/click.js
@@ -5,4 +5,4 @@ msg += 'Our servers are being updated between 3 and 4 a.m. ';
 msg += 'During this time, there may be minor disruptions to service.</div>';
 
 var elNote = document.createElement('div');   // Create a new element
-elNote.setAttribute('id', 'note'); 
+elNote.setAttribute('id', 'note');           // Add an id of note

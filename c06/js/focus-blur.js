da4ca9a810fc6a8df40d4da72305a96f5bdcfebb
CodeXRook <lacekitfly@gmail.com>
2019-09-29 23:32:05 -0400

    textContext

--- c06/js/focus-blur.js
+++ c06/js/focus-blur.js
@@ -2,6 +2,7 @@ function checkUsername(){  // Declare function
     var username  = el.value;  // Store username in variable
     if(username.length < 5){   // If username < 5 characters
         elMsg.className = 'warning';   // Change class on message
+        elMsg.textContent = 'Not long enough, yet...';// Update message
 
     }
 }

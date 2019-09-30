e6f0c5c683ee8cb1cfff7e6a90f28c302aba9724
CodeXRook <lacekitfly@gmail.com>
2019-09-30 02:16:49 -0400

    counter element

--- c06/js/keypress.js
+++ c06/js/keypress.js
@@ -3,5 +3,5 @@ var el;                               // Declare variables
 function charCount(e){                 // Declare function
   var textEntered, charDisplay, counter, lastkey;   // Declare variables
   textEntered = document.getElementById('message').value;  // User's text
-  charDisplay = document.getElementById('charactersLeft');
+  charDisplay = document.getElementById('charactersLeft');  // Counter element
 }

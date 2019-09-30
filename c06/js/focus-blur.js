e57e3172e17437e738e4b58e894c23087dd89fcb
CodeXRook <lacekitfly@gmail.com>
2019-09-29 23:29:00 -0400

    If username value less than

--- c06/js/focus-blur.js
+++ c06/js/focus-blur.js
@@ -1,6 +1,6 @@
 function checkUsername(){  // Declare function
     var username  = el.value;  // Store username in variable
-    if(username.length < 5){
+    if(username.length < 5){   // If username < 5 characters
 
     }
 }

7a5e5b02b58e625f15a7c80abf7593ac740ed9a8
CodeXRook <lacekitfly@gmail.com>
2019-09-29 23:57:26 -0400

    our servers

--- c06/js/click.js
+++ c06/js/click.js
@@ -1,4 +1,4 @@
 // Create the HTML for the message
 var msg = '<div class=\"header\"><a id=\"close\" href="#">close X</a></div>';
 msg += '<div><h2>System Maintenance</h2>';
-msg +=
+msg += 'Our servers are being updated between 3 and 4 a.m. ';

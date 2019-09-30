2ed8866be879f402776d53e92f16e6e866895a88
CodeXRook <lacekitfly@gmail.com>
2019-09-29 23:23:46 -0400

    load out

--- c06/js/load.js
+++ c06/js/load.js
@@ -1,4 +1,5 @@
 function setup() {   // Declare function
     var textInput;    // Create variable
     textInput  = document.getElementById ('username');  // Get username input
+    textInput.focus();                                  // Give username focus
 }

467e50701173011f475d8dc6f617fa38a9ced719
CodeXRook <lacekitfly@gmail.com>
2019-09-30 02:17:12 -0400

    need counter

--- c06/js/keypress.js
+++ c06/js/keypress.js
@@ -4,4 +4,5 @@ function charCount(e){                 // Declare function
   var textEntered, charDisplay, counter, lastkey;   // Declare variables
   textEntered = document.getElementById('message').value;  // User's text
   charDisplay = document.getElementById('charactersLeft');  // Counter element
+  counter
 }

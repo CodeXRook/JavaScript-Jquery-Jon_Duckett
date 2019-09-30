e2b8782b8ce7207e3992fc110cc438393b4b23b9
CodeXRook <lacekitfly@gmail.com>
2019-09-30 19:16:27 -0400

    Add text to li

--- c06/js/mutation.js
+++ c06/js/mutation.js
@@ -8,5 +8,5 @@ function addItem(e){                                       // Declare function
     e.preventDefault();                                    // Prevent link action
     newEl = document.createElement('li');                  // New <li> element
     newText = document.createTextNode('New list item');    // New text node
-    newEl.appendChild(newText); 
+    newEl.appendChild(newText);                            // Add text to <li>
 }

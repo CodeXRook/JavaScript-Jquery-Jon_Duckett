c7c2c2e5bfa6e34687c45f49b01ec45e758a85ab
CodeXRook <lacekitfly@gmail.com>
2019-09-30 19:34:45 -0400

    event

--- c06/js/html5-events.js
+++ c06/js/html5-events.js
@@ -8,4 +8,5 @@ window.addEventListener('DOMContentLoaded', setup, false);
 window.addEventListener('beforeunload', function(event) {
     // This example has been updated to improve cross-browser compatibility (as recommended by MDN)
     var  message = 'You have changes that have not been saved';
+    (event || window.event).returnValue = message;
 }

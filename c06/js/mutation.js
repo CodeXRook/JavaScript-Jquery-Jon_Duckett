eb8fa59be866e7217dcbe36774c4082c37089170
CodeXRook <lacekitfly@gmail.com>
2019-09-30 18:55:18 -0400

    Get item counter

--- c06/js/mutation.js
+++ c06/js/mutation.js
@@ -2,4 +2,4 @@ var elList, addLink, newEl, newText, counter, listItems;  // Declare variables
 
 elList = document.getElementById('list');                  // Get list
 addLink = document.querySelector('a');                     // Get add item button
-counter = document.getElementById('counter');              
+counter = document.getElementById('counter');              // Get item counter

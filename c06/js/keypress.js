d16cb32e75e6bea3b2426ef7f0129d420e36b03e
CodeXRook <lacekitfly@gmail.com>
2019-09-30 02:14:38 -0400

    getEById

--- c06/js/keypress.js
+++ c06/js/keypress.js
@@ -2,5 +2,5 @@ var el;                               // Declare variables
 
 function charCount(e){                 // Declare function
   var textEntered, charDisplay, counter, lastkey;   // Declare variables
-  textEntered
+  textEntered = document.getElementById
 }

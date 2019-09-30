28c97d6be586bf2e8823b4d9d19aa970261bffdf
CodeXRook <lacekitfly@gmail.com>
2019-09-29 23:30:51 -0400

    className

--- c06/js/focus-blur.js
+++ c06/js/focus-blur.js
@@ -1,6 +1,7 @@
 function checkUsername(){  // Declare function
     var username  = el.value;  // Store username in variable
     if(username.length < 5){   // If username < 5 characters
+        elMsg.className = 'warning'; 
 
     }
 }

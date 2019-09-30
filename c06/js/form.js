8895cd495aea0f14a7fa4b5777bdf2dc0200fe01
CodeXRook <lacekitfly@gmail.com>
2019-09-30 02:46:33 -0400

    new ele

--- c06/js/form.js
+++ c06/js/form.js
@@ -1,3 +1,4 @@
 var elForm, elSelectPackage, elPackageHint, elTerms, elTermsHint;  // Declare variables
 elForm         = document.getElementById('formSignup');             // Store elements
-elSelectPackage = document.getElementById
+elSelectPackage = document.getElementById('package')
+elPackageHint

f140df5ee70b32349797c8facf67c757f8dc9503
CodeXRook <lacekitfly@gmail.com>
2019-09-30 02:43:46 -0400

    store elements

--- c06/js/form.js
+++ c06/js/form.js
@@ -1,2 +1,2 @@
 var elForm, elSelectPackage, elPackageHint, elTerms, elTermsHint;  // Declare variables
-elForm         = document.getElementById('formSignup');
+elForm         = document.getElementById('formSignup');             // Store elements

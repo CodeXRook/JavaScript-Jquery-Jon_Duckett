f715aa7bc707e3864a2f1c85208922aaea184652
CodeXRook <lacekitfly@gmail.com>
2019-09-30 02:51:26 -0400

    termHint

--- c06/js/form.js
+++ c06/js/form.js
@@ -1,5 +1,6 @@
 var elForm, elSelectPackage, elPackageHint, elTerms, elTermsHint;  // Declare variables
-elForm         = document.getElementById('formSignup');             // Store elements
+elForm          = document.getElementById('formSignup');             // Store elements
 elSelectPackage = document.getElementById('package')
-elPackageHint    = document.getElementById('packageHint');
-elTerms          = document.getElementById('terms');
+elPackageHint   = document.getElementById('packageHint');
+elTerms         = document.getElementById('terms');
+elTermsHint     = document.getElementById

bf3fd1f841a19fdefce899041ed7e41d8afb2a75
CodeXRook <lacekitfly@gmail.com>
2019-09-30 01:08:13 -0400

    doc cx

--- c06/js/position.js
+++ c06/js/position.js
@@ -2,4 +2,4 @@ var sx = document.getElementById('sx');            // Element to hold screenX
 var sy = document.getElementById('sy');            // Element to hold screenY
 var px = document.getElementById('px');            // Element to hold pageX
 var py = document.getElementById('py');            // Element to hold pageY
-var
+var cx = document.

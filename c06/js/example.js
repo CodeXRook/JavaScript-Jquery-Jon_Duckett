0336d8bdecbd3c546eb9d9b5923c8ae80f99aa62
CodeXRook <lacekitfly@gmail.com>
2019-09-30 20:56:57 -0400

    writeLabel

--- c06/js/example.js
+++ c06/js/example.js
@@ -3,4 +3,4 @@ var noteInput, noteName, textEntered, target;     // Declare variables
 noteName = document.getElementById('noteName');   // Element that holds note
 noteInput  = document.getElementById('noteInput');// Input for writing the note
 
-function
+function writeLabel(e) 

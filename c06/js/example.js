8df0a0c97e329619d6a97f768ce84f4845b60c91
CodeXRook <lacekitfly@gmail.com>
2019-09-30 21:21:20 -0400

    if value is recorded

--- c06/js/example.js
+++ c06/js/example.js
@@ -24,6 +24,6 @@ function recorderControls(e) {                      // Declare recorderControls(
     }
 
     switch(target.getAttribute('data-state')) {    // Get the data-state attribute
-    case 'record':  
+    case 'record':                                 // If its value is record
     }
 }

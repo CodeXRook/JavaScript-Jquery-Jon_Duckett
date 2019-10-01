8b0375680b625775822cd9b2ef77d40a87e74e18
CodeXRook <lacekitfly@gmail.com>
2019-10-01 03:30:46 -0400

    write

--- c07/js/css.js
+++ c07/js/css.js
@@ -1,5 +1,7 @@
 $(function() {
 
     // Get the background color of the first list item.
-    var backgroundColor 
+    var backgroundColor = $('li').css('background-color');
+
+    // Write what the background color was after the list.
 }

798c4e5dd5ec1102f975b1cec6553d44fc7d0713
CodeXRook <lacekitfly@gmail.com>
2019-09-30 20:56:16 -0400

    func

--- c06/js/example.js
+++ c06/js/example.js
@@ -2,3 +2,5 @@ var noteInput, noteName, textEntered, target;     // Declare variables
 
 noteName = document.getElementById('noteName');   // Element that holds note
 noteInput  = document.getElementById('noteInput');// Input for writing the note
+
+function

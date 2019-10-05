2cb139f7bdcc5f73b224cf04030ee2a3059e7cbc
CodeXRook <lacekitfly@gmail.com>
2019-10-05 01:37:28 -0400

    get func

--- c07/js/filters.js
+++ c07/js/filters.js
@@ -4,4 +4,8 @@ $(function() {
   $listItems.filter('.hot:last').removeClass('hot');
   $('li:not(.hot)').addClass('cool');
   $listItems.has('em').addClass('complete');
+
+  $listItems.each(function(){
+      
+  }
 }

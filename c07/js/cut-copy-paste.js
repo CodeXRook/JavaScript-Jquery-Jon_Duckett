68392475b9d147b23829b8e167a4fe033af6e07b
CodeXRook <lacekitfly@gmail.com>
2019-10-05 05:29:36 -0400

    position html

--- c07/js/cut-copy-paste.js
+++ c07/js/cut-copy-paste.js
@@ -1,10 +1,10 @@
 $(function() {
   var $p = $('p');   
 
-  // Create a clone of the <p> element and add it after the
-  <h2> element.
+  // Create a clone of the <p> element and add it after the <h2> element.
     var $clonedQuote = $p.clone();
-    $p.remove();\$clonedQuote.insertAfter('h2');
+    $p.remove();
+    $clonedQuote.insertAfter('h2');
 
     // Remove first item, and add it to the end of the list.
     // Note how this happens in both lists.

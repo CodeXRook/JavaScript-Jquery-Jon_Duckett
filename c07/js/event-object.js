688a5088bb3dc01084f0842f22d3a611aed7160e
CodeXRook <lacekitfly@gmail.com>
2019-10-05 00:46:16 -0400

    event delegation

--- c07/js/event-object.js
+++ c07/js/event-object.js
@@ -1,9 +1,9 @@
-$(function) {
+$(function() {
    $ ('li').on('click', function(e) {
     $('li span').remove();
     var date = new Date();
     date.setTime(e.timeStamp);
     var clicked = date.toDateString();
     $(this).append('<span class="date">' + clicked + ' ' + e.type + '</span>');
-   }
-}
+   });
+});

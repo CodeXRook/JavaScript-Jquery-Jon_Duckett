3b4cb52825c05ceb6dfccd5939ff08cb10e39198
CodeXRook <lacekitfly@gmail.com>
2019-10-05 00:39:37 -0400

    append

--- c07/js/event-object.js
+++ c07/js/event-object.js
@@ -4,5 +4,6 @@ $(function) {
     var date = new Date();
     date.setTime(e.timeStamp);
     var clicked = date.toDateString();
+    $(this).append('<span class="date">' + clicked + ' ' + e.type + '</span>');
    }
 }

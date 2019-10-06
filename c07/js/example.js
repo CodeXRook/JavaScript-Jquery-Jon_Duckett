a738871c39f7a488011f727eeecf141b5c352509
CodeXRook <lacekitfly@gmail.com>
2019-10-06 00:48:25 -0400

    var item

--- c07/js/example.js
+++ c07/js/example.js
@@ -1,5 +1,6 @@
 $(function() {
   // SETUP
   var $list, $newItemForm, $newItemButton;
-  var item = '';             // item is an empty string
+  var item = '';            // item is an empty string
+  $list = $('ul');         // Cache the unordered list
 }

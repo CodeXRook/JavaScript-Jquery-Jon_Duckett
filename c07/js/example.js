b635d0aaa8f0dd5e0c40f0f09c006471b9afd97b
CodeXRook <lacekitfly@gmail.com>
2019-10-06 00:51:25 -0400

    fadeIn

--- c07/js/example.js
+++ c07/js/example.js
@@ -6,4 +6,8 @@ $(function() {
   $newItemForm = $('#newItemForm');    // Cache form to add new items
   $newItemButton = $('#newItemButton');   // Cache button to show form
 
+  $('li').hide().each(function(index) {   // Hide list items
+   $(this).delay(450 * index).fadeIn(1600);  // Then fade them in
+  });
+
 }

0946c306478cc7998c8e2624e7e984a0730211f2
CodeXRook <lacekitfly@gmail.com>
2019-10-06 00:55:59 -0400

    hide form

--- c07/js/example.js
+++ c07/js/example.js
@@ -19,4 +19,5 @@ $(function() {
 
   // SETUP FORM FOR NEW ITEMS
   $newItemButton.show();                         // Show the button
+  $newItemForm.hide();                           // Hide the form
 }

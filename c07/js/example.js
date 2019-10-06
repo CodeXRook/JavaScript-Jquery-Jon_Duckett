aec5db9b441b71ad86720139bc66ee7c1e1238c1
CodeXRook <lacekitfly@gmail.com>
2019-10-06 00:48:59 -0400

    newItemForm

--- c07/js/example.js
+++ c07/js/example.js
@@ -3,4 +3,5 @@ $(function() {
   var $list, $newItemForm, $newItemButton;
   var item = '';            // item is an empty string
   $list = $('ul');         // Cache the unordered list
+  $newItemForm = $('#newItemForm');    // Cache form to add new items
 }

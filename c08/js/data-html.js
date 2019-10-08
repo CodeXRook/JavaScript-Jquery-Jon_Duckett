703a10e468000dd930f2f620a20be93196bfc558
CodeXRook <lacekitfly@gmail.com>
2019-10-07 23:28:11 -0400

    getEl

--- c08/js/data-html.js
+++ c08/js/data-html.js
@@ -2,5 +2,8 @@ var xhr = new XMLHttpRequest();   // Create XMLHttpRequest object
 
 xhr.onload = function() {          // When response has loaded
     // The following conditional check will not work locally - only on a server
-    if(xhr.status === 200) 
+    if(xhr.status === 200) {      // If server status was ok
+        document.getElementById('content').innerHTML = xhr.responseText; // Update
+
+    }
 }

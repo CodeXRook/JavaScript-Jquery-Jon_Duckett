30adc841d5e739e5f4f9f4ab808efcf331171d2d
CodeXRook <lacekitfly@gmail.com>
2019-10-12 02:30:36 -0400

    var response

--- c08/js/data-xml.js
+++ c08/js/data-xml.js
@@ -5,5 +5,9 @@
 var xhr = new XMLHttpRequest();        // Create XMLHttpRequest object
 
 xhr.onload = function(){           // When response has loaded
+    // The following conditional check will not work locally - only on a server
+    // if (xhr.status === 200) {             // If server status was ok
 
+    // THIS PART IS DIFFERENT BECAUSE IT IS PROCESSING XML NOT HTML
+    var response = xhr.responseXML;       // Get XML from the server
 }

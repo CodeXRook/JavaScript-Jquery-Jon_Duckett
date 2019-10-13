1187d7b0398cd7aaf94c07faeb774fe78c6aa0bc
CodeXRook <lacekitfly@gmail.com>
2019-10-13 17:57:13 -0400

    for loop

--- c08/js/data-jsonp.js
+++ c08/js/data-jsonp.js
@@ -1,4 +1,7 @@
 function showEvents(data){       // Callback when JSON loads
     var newContent = '';         // Variable to hold HTML
 
+    // BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
+    for (var i = 0; i < data.events.length; i++) {    // Loop through object
+
 }

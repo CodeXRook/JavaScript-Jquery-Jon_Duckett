862a8535fc73813dbf41eba1714304da1fbbfbd3
CodeXRook <lacekitfly@gmail.com>
2019-10-13 14:35:39 -0400

    onload func

--- c08/js/data-json.js
+++ c08/js/data-json.js
@@ -1,3 +1,7 @@
 // NOTE: If you run this file locally
 // You will not get a server status
 // You can comment out lines 9 and 26 to make it work locally
+
+var xhr = new XMLHttpRequest();                 // Create XMLHttpRequest object
+
+xhr.onload = function() {                       // When readystate changes

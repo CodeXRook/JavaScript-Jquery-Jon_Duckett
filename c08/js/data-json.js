aa105aad712183753caab8f34ba1dede0393ea00
CodeXRook <lacekitfly@gmail.com>
2019-10-13 14:40:14 -0400

    var new content

--- c08/js/data-json.js
+++ c08/js/data-json.js
@@ -5,3 +5,9 @@
 var xhr = new XMLHttpRequest();                 // Create XMLHttpRequest object
 
 xhr.onload = function() {                       // When readystate changes
+    // The following conditional check will not work locally - only on a server
+    //if(xhr.status === 200) {                      // If server status was ok
+    responseObject = JSON.parse(xhr.responseText);
+
+    // BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
+    var newContent = '';

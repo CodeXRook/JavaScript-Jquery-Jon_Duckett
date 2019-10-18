40e3ebc94062affccb483d5ff848d1c56423f88f
CodeXRook <lacekitfly@gmail.com>
2019-10-18 17:53:54 -0400

    newContent

--- c08/js/data-jsonp.js
+++ c08/js/data-jsonp.js
@@ -3,5 +3,8 @@ function showEvents(data){       // Callback when JSON loads
 
     // BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
     for (var i = 0; i < data.events.length; i++) {    // Loop through object
+        newContent += '<div class="event">';
+        newContent += '<img src="' + data.events[i].map + '" ';
+        newContent += '<p><b>' + data.events[i].location + '</b><br>';
 
 }

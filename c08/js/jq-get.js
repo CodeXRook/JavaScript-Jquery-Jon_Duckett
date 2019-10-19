5a4ec183ef2f3ca81eccf6f1c582341e908e348b
CodeXRook <lacekitfly@gmail.com>
2019-10-18 23:37:48 -0400

    jq-get js

---
+++ c08/js/jq-get.js
@@ -0,0 +1,4 @@
+// NOTE: This example will only work if you run it on a web server (it will not work locally)
+
+// This first variable creates the t-shirt options, the HTML for it is shown on p393
+var

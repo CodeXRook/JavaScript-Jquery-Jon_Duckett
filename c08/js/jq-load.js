e3ae8949149a35cd658291603cf95c1dbb575f20
CodeXRook <lacekitfly@gmail.com>
2019-10-18 23:26:18 -0400

    add class

--- c08/js/jq-load.js
+++ c08/js/jq-load.js
@@ -7,3 +7,6 @@
 $('nav a').on('click', function(e) {                 // User clicks nav link
     e.preventDefault();                                // Stop loading new link
     var url = this.href;                               // Get value of href
+
+    $('nav a.current').removeClass('current');         // Clear current indicator
+    $(this).addClass('current');                       // New current indicator

a24a0c3d854095485bd6f556b447cde03b45d8e6
CodeXRook <lacekitfly@gmail.com>
2019-10-18 23:40:04 -0400

    add ratings

--- c08/js/jq-get.js
+++ c08/js/jq-get.js
@@ -4,3 +4,5 @@
 var  vote = '<div id="vote"><div class="third"><a href="http://example.org?tshirt=gray"><img src="img/t-gray.png" id="gray" alt="gray" /></a></div><div class="third"><a
 href="http://example.org?tshirt=yellow" id="yellow"><img src="img/t-yellow.png" id="yellow" alt="yellow" /></a></div><div class="third"><a href="http://example.org?tshirt=green"><img src="img/t-green.png" id="green" alt="green" /></a></div></div>'; 
 $('#selector').append(vote);
+
+// This adds ratings to the side bar

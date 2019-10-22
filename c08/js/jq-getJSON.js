f522e22855834fb2e82bc4cce6649c90218083cc
CodeXRook <lacekitfly@gmail.com>
2019-10-22 17:02:21 -0400

    func

--- c08/js/jq-getJSON.js
+++ c08/js/jq-getJSON.js
@@ -1,5 +1,3 @@
-// NOTE: This example will not work locally in Chrome / IE because of cross-domain restrictions.
-// You can try it out on the website for the book http://javascriptbook.com/code
-// or run it on your own server.
-
 $('#exchangerates').append('<div id="rates"></div><div id="reload"></div>');
+
+function

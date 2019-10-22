6c8608d38c4108b9d8fc0a20908df28dfd778ecb
CodeXRook <lacekitfly@gmail.com>
2019-10-22 17:14:57 -0400

    cache selection

--- c08/js/jq-ajax.js
+++ c08/js/jq-ajax.js
@@ -1,4 +1,5 @@
 $('nav a').on('click', function(e){
 e.preventDefault();
 var url = this.href;                                      // URL to load
+var $content = $('#content');                             // Cache selection
 }

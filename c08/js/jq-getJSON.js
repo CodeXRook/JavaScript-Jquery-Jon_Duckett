715eddfa4b952a7336d5c49b442f378ecb989f77
CodeXRook <lacekitfly@gmail.com>
2019-10-22 17:05:23 -0400

    create new date object

--- c08/js/jq-getJSON.js
+++ c08/js/jq-getJSON.js
@@ -3,4 +3,5 @@ $('#exchangerates').append('<div id="rates"></div><div id="reload"></div>');
 function loadRates(){
     $.getJSON('data/rates.json')
     .done( function(data){                                 // SERVER RETURNS DATA
+        var d = new Date();                                  // Create date object
 }

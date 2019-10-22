2a48fd7f7867164d3696e7e6c6ef835e215bd6d5
CodeXRook <lacekitfly@gmail.com>
2019-10-22 17:03:51 -0400

    server returns data

--- c08/js/jq-getJSON.js
+++ c08/js/jq-getJSON.js
@@ -2,4 +2,5 @@ $('#exchangerates').append('<div id="rates"></div><div id="reload"></div>');
 
 function loadRates(){
     $.getJSON('data/rates.json')
+    .done( function(data){                                 // SERVER RETURNS DATA
 }

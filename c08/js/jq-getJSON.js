f93b03feaaf8b85f0ba7a6161de5d2d06e3dbabe
CodeXRook <lacekitfly@gmail.com>
2019-10-22 17:07:27 -0400

    div class

--- c08/js/jq-getJSON.js
+++ c08/js/jq-getJSON.js
@@ -4,7 +4,9 @@ function loadRates(){
     $.getJSON('data/rates.json')
     .done( function(data){                            // SERVER RETURNS DATA
         var d = new Date();                           // Create date object
-        var hrs = d.getHours();                              // Get hours
-        var mins = d.getMinutes();                           // Get mins
-        var msg = '<h2>Exchange Rates</h2>';                 // Start message
+        var hrs = d.getHours();                       // Get hours
+        var mins = d.getMinutes();                    // Get mins
+        var msg = '<h2>Exchange Rates</h2>';          // Start message
+        $.each(data, function(key, val) {             // Add each rate
+            msg += '<div class="' + key + '">' + key + ': ' + val + '</div>';
 }

fefdf141ca7fa116411dafbbf2a31425894ce0d9
CodeXRook <lacekitfly@gmail.com>
2019-10-23 22:20:50 -0400

    if statement

--- c08/js/example.js
+++ c08/js/example.js
@@ -3,4 +3,5 @@ $(function(){             // When the DOM is ready
   var times;              // Declare global variable
   $.ajax({
     beforeSend: function(xhr) {         // Before requesting data
+        if (xhr.overrideMimeType) {                 // If supported
 }

27c954a5bb918c67352f9c9e8d2ec59888541a53
CodeXRook <lacekitfly@gmail.com>
2019-10-23 23:13:30 -0400

    remove class

--- c08/js/example.js
+++ c08/js/example.js
@@ -57,5 +57,7 @@ $(function(){             // When the DOM is ready
  $('nav a').on('click', function(e) {                       // Click on nav
     e.preventDefault();                                      // Prevent loading
     var url = this.href;                                     // Get URL to load
+
+    $('nav a.current').removeClass('current');               // Update nav
   }
 }

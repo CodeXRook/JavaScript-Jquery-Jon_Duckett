a86b357e8c7b0e780150792cfdd459b0b24639c8
CodeXRook <lacekitfly@gmail.com>
2019-11-03 12:56:40 -0500

    Cache the div

--- c09/form-init.js
+++ c09/form-init.js
@@ -3,4 +3,5 @@ $(function() {
     $('#arrival').datepicker();      // Turn input to JQUI datepicker
 
     var $amount = $('#amount');      // Cache the price input
+    var $range = $('#price-range');  // Cache the <div> for the price range
 }

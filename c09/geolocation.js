0bda79512c1108dd939bbc75b138c8205e39543e
CodeXRook <lacekitfly@gmail.com>
2019-11-03 10:17:53 -0500

    elMap

--- c09/geolocation.js
+++ c09/geolocation.js
@@ -2,4 +2,4 @@
 // In other browsers, you may get a prompt that allows you to share location data.
 // It may take the browser a while to determine your location (his demonstrates why you should not depend on this information before loading the entire page).
 
-var elMap
+var elMap  = document.getElementById('loc');                 // HTML element

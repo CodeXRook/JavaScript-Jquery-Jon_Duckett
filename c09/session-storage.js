1ad0376e1371993f5b82d2948b44e43559644e7d
CodeXRook <lacekitfly@gmail.com>
2019-11-03 11:38:59 -0500

    ele populated

--- c09/session-storage.js
+++ c09/session-storage.js
@@ -3,4 +3,6 @@ if (Modernizr.sessionstorage) {
 
     var txtUsername = document.getElementById('username'),  // Get form elements
     txtAnswer = document.getElementById('answer');
+
+    txtUsername.value = sessionStorage.getItem('username'); // Elements populated
 }

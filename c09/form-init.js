29d901c69ea73f799c63588e853d5c9c312e2d7f
CodeXRook <lacekitfly@gmail.com>
2019-11-03 13:08:33 -0500

    when slider used update

--- c09/form-init.js
+++ c09/form-init.js
@@ -9,4 +9,5 @@ $(function() {
         min: 0,                        // Minimum value
         max: 400,                      // Maximum value
         values: [175, 300],            // Values to use when the page loads 
+        slide: function(event, ui) {   // When slider used update amount element
 }

76d62c3b43a5e570580be9a606f02e981d4658a2
CodeXRook <lacekitfly@gmail.com>
2019-11-02 23:42:43 -0400

    APIs

---
+++ c09/geolocation.js
@@ -0,0 +1,5 @@
+// Note that this example might not work locally in Chrome (it works on a web server).
+// In other browsers, you may get a prompt that allows you to share location data.
+// It may take the browser a while to determine your location (his demonstrates why you should not depend on this information before loading the entire page).
+
+var elMap

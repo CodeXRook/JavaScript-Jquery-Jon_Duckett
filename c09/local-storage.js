24de21e0008eaa020f6ff9ad6307fa4242c472e9
CodeXRook <lacekitfly@gmail.com>
2019-11-03 11:32:04 -0500

    get elements

--- c09/local-storage.js
+++ c09/local-storage.js
@@ -1,5 +1,5 @@
 // This example has been updated to use Modernizr not window- please note the use of localstorage in lowercase
 if (Modernizr.localstorage){
 
-    var txtUsername 
+    var txtUsername = document.getElementById('username'); // Get form elements
 }

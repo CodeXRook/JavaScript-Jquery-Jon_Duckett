f4aa0fba1f16837a4d95da971e8512dccc8ebd8c
CodeXRook <lacekitfly@gmail.com>
2019-11-03 12:10:50 -0500

    jquery func

--- c09/history.js
+++ c09/history.js
@@ -1,4 +1,4 @@
 // NOTE: Because this example uses jQuery's load() method (as introduced in the previous chapter)
 // It will not work locally in some browsers 
 // or run it on your own web server
-$
+$(function()

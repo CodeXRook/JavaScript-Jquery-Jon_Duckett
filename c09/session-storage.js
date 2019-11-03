2eb0bcbb1e55c86da3304a4db9064cda4004ce0d
CodeXRook <lacekitfly@gmail.com>
2019-11-03 11:37:48 -0500

    var username

--- c09/session-storage.js
+++ c09/session-storage.js
@@ -1,2 +1,5 @@
 // This example has been updated to use Modernizr - please note the use of of lowercase in sessionstorage
-if (Modernizr.sessionstorage)
+if (Modernizr.sessionstorage) {
+
+    var txtUsername
+}

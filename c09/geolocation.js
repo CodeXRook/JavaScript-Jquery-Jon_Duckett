ccc465a8ce7fd1f5d0656095efa682f566c264c0
CodeXRook <lacekitfly@gmail.com>
2019-11-03 10:38:55 -0500

    add latitude

--- c09/geolocation.js
+++ c09/geolocation.js
@@ -16,6 +16,7 @@ function success(position) {                                // Got location
     msg = '<h3>Longitude:<br>';                               // Create message
     msg += position.coords.longitude + '</h3>';               // Add longitude
     msg += '<h3>Latitude:<br>';                               // Create message
+    msg += position.coords.latitude + '</h3>';                // Add latitude
     
   
   

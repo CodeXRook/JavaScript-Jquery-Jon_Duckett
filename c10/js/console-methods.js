f386894027b2e7e9b7d5b2d8ef944536c9b0c8a3
CodeXRook <lacekitfly@gmail.com>
2019-11-03 23:42:59 -0500

    On blur

--- c10/js/console-methods.js
+++ c10/js/console-methods.js
@@ -1,3 +1,6 @@
 console.info('And we\'re off...');                    // Info: script running
 
 var $form, width, height, area;
+$form = $('#calculator');
+
+$('form input[type="text"]').on('blur', function() {  // On blur event

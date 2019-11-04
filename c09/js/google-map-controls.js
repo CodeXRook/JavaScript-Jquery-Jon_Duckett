f9e2ec73a7613433ca88a046e80d3d6a0f0f695a
CodeXRook <lacekitfly@gmail.com>
2019-11-03 22:23:17 -0500

    script src

--- c09/js/google-map-controls.js
+++ c09/js/google-map-controls.js
@@ -28,4 +28,5 @@ function  init() {
 
 function loadScript() {
     var script = document.createElement('script');
+    script.src = 'http://maps.googleapis.com/maps/api/js?sensor=false&callback=init';
 }

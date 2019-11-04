dd34454dbdba27c015431c9e6afdaa759ecf922f
CodeXRook <lacekitfly@gmail.com>
2019-11-04 00:25:51 -0500

    title

--- c10/js/try-catch-finally.js
+++ c10/js/try-catch-finally.js
@@ -1,4 +1,5 @@
 var feed = document.getElementById('feed');
 
 // Correct feed
-var response 
+var response = '{"deals": [{"title": "Farrow and Ball","description": "New season 2.5l tins are all reduced this week","price": 30,"link": "http://www.example.com/farrow-and-ball/"},
+{"title": "Siecle Paints from the UK","description": "Probably the best named paints in the world","price": 28,"link": "http://www.example.com/siecle/"},{"title": "Kelly Hoppen","description": "Now stocking paints by interior designer Kelly Hoppen","price": 42,"link": "http://www.example.com/kelly-hoppen/"}]}';

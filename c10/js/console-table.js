febe6582465ae58a943db4ac812d8aedf27eb4e5
CodeXRook <lacekitfly@gmail.com>
2019-11-03 23:57:15 -0500

    new tel

--- c10/js/console-table.js
+++ c10/js/console-table.js
@@ -3,6 +3,6 @@ var contacts  = {                 // Store contact info in an object literal
         "Tel": "+44 (0)207 946 0128",
         "Country": "UK"},
     "Sydney":{
-        
+        "Tel": "+61 (0)2 7010 1212",
     }
     }

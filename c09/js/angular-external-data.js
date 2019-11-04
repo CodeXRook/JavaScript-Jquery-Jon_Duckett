30d4c30f4553914e21c04804fee22c99ba637fc5
CodeXRook <lacekitfly@gmail.com>
2019-11-03 19:55:36 -0500

    error func

--- c09/js/angular-external-data.js
+++ c09/js/angular-external-data.js
@@ -3,4 +3,5 @@
 function TimetableCtrl($scope, $http){
     $http.get('js/items.json')
     .success(function(data) { $scope.sessions = data.sessions; })
+    .error(function(data) { console.log('error'); });
 }

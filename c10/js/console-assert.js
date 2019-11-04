a911a61eb6a429119e384e6b27db2a465bc2f340
CodeXRook <lacekitfly@gmail.com>
2019-11-04 00:06:30 -0500

    func

--- c10/js/console-assert.js
+++ c10/js/console-assert.js
@@ -5,3 +5,5 @@ $('form input[type="text"]').on('blur', function() {
    // The message only shows if user has entered number less than 10
    console.assert(this.value > 10, 'User entered less than 10');
 });
+
+$('#calculator').on('submit', function(e)

530a060d9e36bfe9d6caa64f7ebec94221a0e90b
CodeXRook <lacekitfly@gmail.com>
2019-11-03 20:56:15 -0500

    draws map

--- c09/js/google-map.js
+++ c09/js/google-map.js
@@ -2,5 +2,8 @@ function init() {
     var mapOptions ={                // Set up the map options
       center: new google.maps.LatLng(40.782710,-73.965310),
       mapTypeId: google.maps.MapTypeId.ROADMAP,
-    }
+      zoom: 13
+    };
+    var venueMap;                                      // Map() draws a map
+ 
 }

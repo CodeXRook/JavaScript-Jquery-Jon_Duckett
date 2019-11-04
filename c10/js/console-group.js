0482663c3e3bdc23b9907cfdcec470c04a856b27
CodeXRook <lacekitfly@gmail.com>
2019-11-03 23:48:06 -0500

    console Show the button was clicked

--- c10/js/console-group.js
+++ c10/js/console-group.js
@@ -2,3 +2,4 @@ var $form = $('#calculator');
 
 $form.on('submit', function(e) {                 // Runs when submit is pressed
     e.preventDefault();
+    console.log('Clicked submit...');              // Show the button was clicked

28b5020b07bf29e23fe300426145dbffdf36edca
CodeXRook <lacekitfly@gmail.com>
2019-11-04 00:55:33 -0500

    if statement

--- c10/js/throw.js
+++ c10/js/throw.js
@@ -4,4 +4,5 @@ var height = 'test';  // height variable
 function calculateArea(width, height) {}
 try {
     var area = width * height;                      // Try to calculate area
+    if (!isNaN(area)) {                             // If it is a number
 }

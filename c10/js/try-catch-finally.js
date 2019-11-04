3b175f722f1cddb889c4e1ec3a046b3b946edca4
CodeXRook <lacekitfly@gmail.com>
2019-11-04 00:29:24 -0500

    JSON

--- c10/js/try-catch-finally.js
+++ c10/js/try-catch-finally.js
@@ -3,3 +3,5 @@ var feed = document.getElementById('feed');
 // Correct feed
 var response = '{"deals": [{"title": "Farrow and Ball","description": "New season 2.5l tins are all reduced this week","price": 30,"link": "http://www.example.com/farrow-and-ball/"},
 {"title": "Siecle Paints from the UK","description": "Probably the best named paints in the world","price": 28,"link": "http://www.example.com/siecle/"},{"title": "Kelly Hoppen","description": "Now stocking paints by interior designer Kelly Hoppen","price": 42,"link": "http://www.example.com/kelly-hoppen/"}]}';
+// Feed with errors - comment the next line out to see it working
+response = '{"deals": [{"title": "Farrow and Ball","description": "New season 2.5l '; // JSON data

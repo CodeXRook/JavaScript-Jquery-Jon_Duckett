eb8ed6fa3d2629a54c8be487d613768952df25ad
CodeXRook <lacekitfly@gmail.com>
2019-11-03 23:36:56 -0500

    console

--- c10/js/console-log.js
+++ c10/js/console-log.js
@@ -3,3 +3,4 @@ var $form, width, height, area;
 $form = $('#calculator');
 
 $('form input[type="text"]').on('blur', function() { // When input loses focus
+    console.log('You entered ', this.value );          // Write value to console

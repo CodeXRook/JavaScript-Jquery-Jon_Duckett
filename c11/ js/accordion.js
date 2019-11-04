575c69931e428c501c11b5af4a7a3a3f13ee56f2
CodeXRook <lacekitfly@gmail.com>
2019-11-04 18:14:08 -0500

    slideToggle

--- c11/ js/accordion.js
+++ c11/ js/accordion.js
@@ -3,3 +3,5 @@ $('.accordion').on('click', '.accordion-control', function(e){ // When clicked
     $(this)                                // Get the element the user clicked on
     .next('.accordion-panel')            // Select following panel 
     .not(':animated')                    // If it is not currently animating
+    .slideToggle();                      // Use slide toggle to show or hide it
+});

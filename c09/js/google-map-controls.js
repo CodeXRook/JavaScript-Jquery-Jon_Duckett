ad51a3ad18db9938d03e884fc11e7327a98b7e7a
CodeXRook <lacekitfly@gmail.com>
2019-11-03 21:34:26 -0500

    center

--- c09/js/google-map-controls.js
+++ c09/js/google-map-controls.js
@@ -2,5 +2,6 @@ function  init() {
     // the mapOptions object contains the information to initialise the map to how we want it
     var mapOptions = {
         zoom: 14,
+        center: new google.maps.LatLng(40.782710,-73.965310),
     }
 }

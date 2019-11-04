03c333a59196a11a79fc5aab77fe3202cf838118
CodeXRook <lacekitfly@gmail.com>
2019-11-03 23:37:27 -0500

    submit

--- c10/js/console-log.js
+++ c10/js/console-log.js
@@ -4,3 +4,6 @@ $form = $('#calculator');
 
 $('form input[type="text"]').on('blur', function() { // When input loses focus
     console.log('You entered ', this.value );          // Write value to console
+});
+
+$('#calculator').on('submit', function(e) {     // When the user clicks submit

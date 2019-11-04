90d10f069c5edfb8fe4236cd265aad9c69933919
CodeXRook <lacekitfly@gmail.com>
2019-11-03 19:54:15 -0500

    js items

--- c09/js/angular-external-data.js
+++ c09/js/angular-external-data.js
@@ -1,3 +1,5 @@
 // NOTE: Because this example loads external data it will not work locally in some browsers
 // You can try it online at http://javascriptbook.com/code/c09 
-function 
+function TimetableCtrl($scope, $http){
+    $http.get('js/items.json')
+}

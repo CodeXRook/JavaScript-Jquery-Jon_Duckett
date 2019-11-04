62b8692fe05762535f20968a107729c0b11affbd
CodeXRook <lacekitfly@gmail.com>
2019-11-03 23:43:19 -0500

    Warn what was entered

--- c10/js/console-methods.js
+++ c10/js/console-methods.js
@@ -4,3 +4,5 @@ var $form, width, height, area;
 $form = $('#calculator');
 
 $('form input[type="text"]').on('blur', function() {  // On blur event
+    console.warn('You entered ', this.value);           // Warn: what was entered
+});

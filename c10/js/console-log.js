8ab34723299d2503e6110cb2d74fd12fab8a83d7
CodeXRook <lacekitfly@gmail.com>
2019-11-03 23:36:33 -0500

    when input loses focus

--- c10/js/console-log.js
+++ c10/js/console-log.js
@@ -1,3 +1,5 @@
 console.log('And we\'re off...');               // Indicates script is running
 var $form, width, height, area;
 $form = $('#calculator');
+
+$('form input[type="text"]').on('blur', function() { // When input loses focus

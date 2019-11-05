63407edd067fd9618cd0656d3207899c69977c6f
CodeXRook <lacekitfly@gmail.com>
2019-11-05 02:56:41 -0500

    Get link active

--- c11/ js/tabs.js
+++ c11/ js/tabs.js
@@ -1,3 +1,4 @@
 $('.tab-list').each(function(){             // Find lists of tabs
   var $this = $(this);                            // Store this list
   var $tab = $this.find('li.active');             // Get the active list item
+  var $link = $tab.find('a');                     // Get link from active tab

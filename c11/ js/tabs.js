1091d56a5bf6c6ee34581f466d57af7477379a4b
CodeXRook <lacekitfly@gmail.com>
2019-11-05 02:57:24 -0500

    when click on tab

--- c11/ js/tabs.js
+++ c11/ js/tabs.js
@@ -3,3 +3,5 @@ $('.tab-list').each(function(){             // Find lists of tabs
   var $tab = $this.find('li.active');             // Get the active list item
   var $link = $tab.find('a');                     // Get link from active tab
   var $panel = $($link.attr('href'));             // Get active panel
+
+  $this.on('click', '.tab-control', function(e) { // When click on a tab

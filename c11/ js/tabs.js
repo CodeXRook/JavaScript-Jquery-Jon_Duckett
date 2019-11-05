d0f26f3d5abbfde457957848a9d382a33645084f
CodeXRook <lacekitfly@gmail.com>
2019-11-05 02:56:16 -0500

    active list

--- c11/ js/tabs.js
+++ c11/ js/tabs.js
@@ -1,2 +1,3 @@
 $('.tab-list').each(function(){             // Find lists of tabs
   var $this = $(this);                            // Store this list
+  var $tab = $this.find('li.active');             // Get the active list item

64208ed65a7fc8166606f75b27045803f587dd08
CodeXRook <lacekitfly@gmail.com>
2019-11-06 14:12:18 -0500

    Add title if one was given in link

--- c11/ js/photo-viewer.js
+++ c11/ js/photo-viewer.js
@@ -63,3 +63,4 @@ if (cache.hasOwnProperty(src)) {        // If cache contains this image
 
     $img.attr({                           // Set attributes on <img> element
         'src': src,                         // Add src attribute to load image
+        'alt': this.title || ''             // Add title if one was given in link

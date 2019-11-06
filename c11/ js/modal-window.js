c8e77c72d681b36598fb981d171774be34828c6d
CodeXRook <lacekitfly@gmail.com>
2019-11-06 01:29:06 -0500

    new varibale close button

--- c11/ js/modal-window.js
+++ c11/ js/modal-window.js
@@ -2,4 +2,5 @@ var modal = (function() {            // Declare modal object
     var $window = $(window);                        // Store the window
     var $modal = $('<div class="modal"/>');         // Create markup for modal
     var $content = $('<div class="modal-content"/>');
+    var $close = $('<button role="button" class="modal-close">close</button>');
 }

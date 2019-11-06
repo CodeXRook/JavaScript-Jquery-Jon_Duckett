7999a58de887333c00998e8234332d21844688b6
CodeXRook <lacekitfly@gmail.com>
2019-11-06 13:40:23 -0500

    new thumb nail

--- c11/ js/photo-viewer.js
+++ c11/ js/photo-viewer.js
@@ -2,3 +2,4 @@ var request;   // Latest image to be requested
 var $current;   // Image currently being shown 
 var cache = {};    // Cache object
 var $frame = $('#photo-viewer');     // Container for image
+var $thumbs

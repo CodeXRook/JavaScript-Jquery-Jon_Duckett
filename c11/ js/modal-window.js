9674c9affdddf69e52438bc889d5d695cd49aa14
CodeXRook <lacekitfly@gmail.com>
2019-11-06 01:28:32 -0500

    var content

--- c11/ js/modal-window.js
+++ c11/ js/modal-window.js
@@ -1,4 +1,5 @@
 var modal = (function() {            // Declare modal object
     var $window = $(window);                        // Store the window
     var $modal = $('<div class="modal"/>');         // Create markup for modal
+    var $content = $('<div class="modal-content"/>');
 }

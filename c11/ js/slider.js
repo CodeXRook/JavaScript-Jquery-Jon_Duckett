e1c3ac75e88ef1ce43b1b5e76d32cc75a25b3f7a
CodeXRook <lacekitfly@gmail.com>
2019-11-06 23:50:00 -0500

    Create jQuery object

--- c11/ js/slider.js
+++ c11/ js/slider.js
@@ -1,3 +1,4 @@
 $('.slider').each(function() {              // For every slider
   var $this   = $(this);                    // Current slider
   var $group  = $this.find('.slide-group'); // Get the slide-group (container)
+  var $slides = $this.find('.slide');       // Create jQuery object to hold all slides

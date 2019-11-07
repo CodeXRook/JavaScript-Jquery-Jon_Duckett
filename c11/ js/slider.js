d24e7ef626e9eb1e8f2815b6f507092188700d9d
CodeXRook <lacekitfly@gmail.com>
2019-11-06 23:49:27 -0500

    Get the slide-group

--- c11/ js/slider.js
+++ c11/ js/slider.js
@@ -1,2 +1,3 @@
 $('.slider').each(function() {              // For every slider
   var $this   = $(this);                    // Current slider
+  var $group  = $this.find('.slide-group'); // Get the slide-group (container)

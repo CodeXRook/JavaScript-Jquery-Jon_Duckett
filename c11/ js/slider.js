76cb483133908cfcfd8865d54451f7a0df12abe6
CodeXRook <lacekitfly@gmail.com>
2019-11-06 23:50:33 -0500

    Create array to hold navigation

--- c11/ js/slider.js
+++ c11/ js/slider.js
@@ -2,3 +2,4 @@ $('.slider').each(function() {              // For every slider
   var $this   = $(this);                    // Current slider
   var $group  = $this.find('.slide-group'); // Get the slide-group (container)
   var $slides = $this.find('.slide');       // Create jQuery object to hold all slides
+  var buttonArray  = [];                    // Create array to hold navigation buttons

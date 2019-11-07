d4d89c2a6ceca504be28277881ecf703e198ab79
CodeXRook <lacekitfly@gmail.com>
2019-11-07 00:28:07 -0500

    Add it to the button array

--- c11/ js/slider.js
+++ c11/ js/slider.js
@@ -57,3 +57,5 @@ if (index === currentIndex) {    // If index is the current item
 $button.on('click', function() { // Create event handler for the button
     move(index);                   // It calls the move() function
 }).appendTo('.slide-buttons');   // Add to the buttons holder
+buttonArray.push($button);       // Add it to the button array
+});

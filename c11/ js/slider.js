cec8b460606cead4c7781b8d29c5f74c3c17b7cd
CodeXRook <lacekitfly@gmail.com>
2019-11-06 23:52:30 -0500

    Set gap between auto-sliding

--- c11/ js/slider.js
+++ c11/ js/slider.js
@@ -4,3 +4,4 @@ $('.slider').each(function() {              // For every slider
   var $slides = $this.find('.slide');       // Create jQuery object to hold all slides
   var buttonArray  = [];                    // Create array to hold navigation buttons
   var currentIndex = 0;                     // Hold index number of the current slide
+  var timeout;                              // Sets gap between auto-sliding

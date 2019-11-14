75be04cb6c8fd263c1e699145e6dd551a39d04c3
CodeXRook <lacekitfly@gmail.com>
2019-11-13 20:32:03 -0500

    new name

--- c12/js/filter-filter.js
+++ c12/js/filter-filter.js
@@ -2,7 +2,16 @@ $(function () {
     // STORE EACH PERSON AS AN OBJECT IN AN ARRAY 
     var people = [
         {
-            
+         name: 'Casey',                 // Each person is an object 
+         rate:60                        // It holds name and rate
+        },
+        {
+            name:'Camile',
+            rate:80
+        },
+        {
+            name: 'Gordon'
         }
+    
     ]
 })

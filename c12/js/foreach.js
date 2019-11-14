7377e1a64d85ad992042d67891ffaef9c7419192
CodeXRook <lacekitfly@gmail.com>
2019-11-13 20:11:18 -0500

    loop through new array

--- c12/js/foreach.js
+++ c12/js/foreach.js
@@ -15,4 +15,16 @@ $(function(){
            rate: 120
         }
     ];
+
+    // CHECKS EACH PERSO AND ADDS THOSE IN RANGE TO ARRAY
+    var results =[];                         // Array for people in range
+    people.forEach(function(person) {        // For each person
+        if (person.rate >= 65 && person.rate <= 90) { // Is rate in range 
+            results.push(person);                      // If yes add to array
+        }
+});
+
+    // LOOP THROUGH NEW ARRAY AND ADD MATCHING PEOPLE TO THE RESULTS TABLE
+    
+    
 })

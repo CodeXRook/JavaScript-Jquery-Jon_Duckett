f4315bcdad42f0161d9850fcf5123fcd22d0d945
CodeXRook <lacekitfly@gmail.com>
2019-11-13 20:25:08 -0500

    add tbody aftet head

--- c12/js/foreach.js
+++ c12/js/foreach.js
@@ -28,7 +28,10 @@ $(function(){
     var $tableBody = $('<tbody></tbody>');         // New table body
     for (var i = 0; i < results.length; i++) {      // Loop through matches
         var person = results[i];                    // Store current person
-
+        var $row = $('<tr></tr>');                  // Create a row for them
+        $row.append($('<td></td>').text(person.name)); // Add their name
+        $row.append($('<td></td>').text(person.rate)); // Add their rate
+        $tableBody.append( $row );                     // Add row to new content
     }
-    
-})
+    $('thead').after($tableBody);                       // Add tbody after head
+});

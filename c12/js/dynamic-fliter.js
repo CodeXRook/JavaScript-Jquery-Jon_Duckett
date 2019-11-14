461873086388f6b875593025d1b8c785f015977f
CodeXRook <lacekitfly@gmail.com>
2019-11-13 21:06:16 -0500

    name camile

--- c12/js/dynamic-fliter.js
+++ c12/js/dynamic-fliter.js
@@ -3,7 +3,15 @@
     // STORE EACH PERSON AS AN OBJECT IN AN ARRAY
     var people =[
         {
-            name: 'Casey'               // Each person is an object
+            name: 'Casey',              // Each person is an object
+            rate: 60                    // It holds name and rate
+        },
+        {
+            name: 'Camile',
+            rate:80
+        },
+        {
+            
         }
     ]
 })

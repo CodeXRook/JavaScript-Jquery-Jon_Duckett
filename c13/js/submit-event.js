6fde160dfe75c3c71be3607ddad3da8136a1d781
CodeXRook <lacekitfly@gmail.com>
2019-11-29 13:27:58 -0500

    Get all form elements

--- c13/js/submit-event.js
+++ c13/js/submit-event.js
@@ -3,3 +3,4 @@ var form = document.getElementById('login');       // Get form element
 
 addEvent(form, 'submit', function(e) {             // When user submits form
     e.preventDefault();                              // Stop it being sent
+    var elements = this.elements;                    // Get all form elements

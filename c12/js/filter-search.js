32bd52a0e4d8758415dac5e06f94e53b3dbf845f
CodeXRook <lacekitfly@gmail.com>
2019-11-28 20:26:23 -0500

    Get the input element

--- c12/js/filter-search.js
+++ c12/js/filter-search.js
@@ -1,3 +1,4 @@
 (function(){                              // Lives in an IIFE
     var $imgs = $('#gallery img');          // Get the images
+    var $search = $('#filter-search');      // Get the input element
 }

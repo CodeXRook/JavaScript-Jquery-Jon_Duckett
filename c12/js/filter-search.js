1507cee75c29d4496a9e7175cd0c481e91b3dd0e
CodeXRook <lacekitfly@gmail.com>
2019-11-28 20:26:51 -0500

    Create an array called cache

--- c12/js/filter-search.js
+++ c12/js/filter-search.js
@@ -1,4 +1,5 @@
 (function(){                              // Lives in an IIFE
     var $imgs = $('#gallery img');          // Get the images
     var $search = $('#filter-search');      // Get the input element
+    var cache = [];                         // Create an array called cache
 }

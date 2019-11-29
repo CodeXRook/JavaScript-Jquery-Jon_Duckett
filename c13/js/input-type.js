44010d1820384879d2f62f4acccc4ec544d1b862
CodeXRook <lacekitfly@gmail.com>
2019-11-29 13:33:52 -0500

    git element

--- c13/js/input-type.js
+++ c13/js/input-type.js
@@ -3,4 +3,6 @@
     var pwd = document.getElementById('pwd');     // Get password input
     var chk = document.getElementById('showPwd'); // Get checkbox
 
-    addEvent(chk, 'change', function(e) 
+    addEvent(chk, 'change', function(e)  {        // When user clicks on checkbox
+        var target = e.target || e.srcElement;      // Get that element
+    }

a0981d01d20f7d36c94fb646bc786284aea0e2e1
CodeXRook <lacekitfly@gmail.com>
2019-11-29 13:23:16 -0500

    add false

--- c13/js/utilities.js
+++ c13/js/utilities.js
@@ -1,3 +1,4 @@
 // Helper function to add an event listener
 function addEvent (el, event, callback) 
 if ('addEventListener' in el) {                  // If addEventListener works
+    el.addEventListener(event, callback, false);   // Use it

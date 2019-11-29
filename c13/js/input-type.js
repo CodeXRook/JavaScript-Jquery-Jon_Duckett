158b5bd0e86b07c32ec7784036fb510b7256fb11
CodeXRook <lacekitfly@gmail.com>
2019-11-29 13:33:08 -0500

    change func

--- c13/js/input-type.js
+++ c13/js/input-type.js
@@ -2,3 +2,5 @@
 
     var pwd = document.getElementById('pwd');     // Get password input
     var chk = document.getElementById('showPwd'); // Get checkbox
+
+    addEvent(chk, 'change', function(e) 

c167a403c4e6ee2956af81c683afd79d4461edfd
CodeXRook <lacekitfly@gmail.com>
2019-11-28 20:39:06 -0500

    Remove The start

--- c12/js/sort-table.js
+++ c12/js/sort-table.js
@@ -1,4 +1,6 @@
 var compare = {                             // Declare compare object
     name: function(a, b) {                  // Add a method called name
         a = a.replace(/^the /i, '');        // Remove The from start of parameter
+        b = b.replace(/^the /i, '');          // Remove The from start of parameter
+
 }
